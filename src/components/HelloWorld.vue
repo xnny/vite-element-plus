cd98d4f904a34ca0af6cf83b7730bc44230ac406
tto <iwangtao@gmail.com>
2021-11-28 20:10:02 +0800

    添加测试文件；

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-button @click="testMsg">Click</el-button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import 'element-plus/theme-chalk/el-message.css';
+import { ElMessage } from 'element-plus';
+
+export default defineComponent({
+  name: "HelloWorld",
+  methods: {
+    testMsg() {
+      ElMessage({
+        showClose: true,
+        message: 'test',
+        center: true,
+        type: 'success',
+      });
+    }
+  }
+});
+</script>
